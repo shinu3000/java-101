c60b7525c0f71ccf63a6fec9c20e4036846eea64
Hunter-X-Hunter <srinivasnen3000@gmail.com>
2020-08-28 17:53:39 +0530

    My initial release

---
+++ Adder/src/com/adder/pro/AdderLogic.java
@@ -0,0 +1,12 @@
+package com.adder.pro;
+
+public class AdderLogic {
+	int c;
+	public AdderLogic(int a, int b) {
+		System.out.println("just entered the contructor");
+		c=a+b;
+		System.out.println("added value is"+c);
+		
+	}
+	
+}
---
+++ Adder/src/com/adder/pro/AdderMain.java
@@ -0,0 +1,10 @@
+package com.adder.pro;
+
+public class AdderMain {
+	public static void main(String args[]) {
+		int a=20;
+		int b=30;
+		new AdderLogic(a ,b);
+		System.out.println("returnd to the main ");
+	}
+}
